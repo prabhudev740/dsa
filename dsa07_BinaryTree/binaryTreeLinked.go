0aafbb0ae3a3bcc351ca1bae8f6f6ef7347d7265
Prabhu <prabhuprasadpanda740@gmail.com>
2022-11-21 20:50:41 +0530

    adding git ignore and updated binaryTree

--- dsa07_BinaryTree/binaryTreeLinked.go
+++ dsa07_BinaryTree/binaryTreeLinked.go
@@ -2,70 +2,94 @@ package main
 
 import "fmt"
 
+// func main() {
+
+// left := TreeNode{}
+// right := TreeNode{}
+// T := BinaryTree{}
+
+// left := makeTree(20, nil, nil)
+// right := makeTree(30, nil, nil)
+// T := makeTree(10, &left, &right)
+
+// fmt.Printf("%T", T)
+// fmt.Println(T.element)
+// fmt.Println(T.left.element)
+// fmt.Println(T.right.element)
+
+// T.preorder()
+// }
+
+// type TreeNode struct {
+// 	element int
+// 	left    *TreeNode
+// 	right   *TreeNode
+// }
+
+// func makeTree(element int, left *TreeNode, right *TreeNode) TreeNode {
+// 	return TreeNode{element, left, right}
+// }
+
+// func (T *TreeNode) preorder() {
+// 	if T != nil {
+// 		fmt.Print(T.element, " ")
+// 		T.left.preorder()
+// 		T.right.preorder()
+// 	}
+// }
+
 func main() {
-	fmt.Println("Binary tree")
+	fmt.Println("Binary Tree")
 	T := BinaryTree{}
 	left := BinaryTree{}
 	right := BinaryTree{}
+
 	left.makeTree(20, nil, nil)
 	right.makeTree(30, nil, nil)
 	T.makeTree(10, &left, &right)
-	T.inorder(T.root)
-	fmt.Println(T.root.left)
 
-	// left := TreeNode{}
-	// right := TreeNode{}
-	// T := BinaryTree{}
+	T.preorder(&T)
+	fmt.Println("")
+	T.inorder(&T)
+	fmt.Println("")
+	T.postorder(&T)
 
-	// left := makeTree(20, nil, nil)
-	// right := makeTree(30, nil, nil)
-	// T := makeTree(10, &left, &right)
-
-	// fmt.Printf("%T", T)
-	// fmt.Println(T.element)
-	// fmt.Println(T.left.element)
-	// fmt.Println(T.right.element)
-
-	// T.preorder()
 }
 
 type TreeNode struct {
 	element int
-	left    *TreeNode
-	right   *TreeNode
+	left    *BinaryTree
+	right   *BinaryTree
 }
 
 type BinaryTree struct {
 	root *TreeNode
 }
 
-// func makeTree(element int, left *TreeNode, right *TreeNode) TreeNode {
-// 	return TreeNode{element, left, right}
-// }
-func (B BinaryTree) makeTree(element int, left *BinaryTree, right *BinaryTree) {
-	B.root = &TreeNode{element, left.root, right.root}
+func (B *BinaryTree) makeTree(ele int, left *BinaryTree, right *BinaryTree) {
+	B.root = &TreeNode{ele, left, right}
 }
 
-func (T *TreeNode) preorder() {
-	if T != nil {
-		fmt.Print(T.element, " ")
-		T.left.preorder()
-		T.right.preorder()
+func (B BinaryTree) preorder(troot *BinaryTree) {
+	if troot != nil {
+		fmt.Print(troot.root.element, " ")
+		B.preorder(troot.root.left)
+		B.preorder(troot.root.right)
 	}
 }
 
-func (B BinaryTree) inorder(root *TreeNode) {
-	if root != nil {
-		B.inorder(root.left)
-		fmt.Println(root.element)
-		B.inorder(root.right)
+func (B BinaryTree) inorder(troot *BinaryTree) {
+	if troot != nil {
+		B.preorder(troot.root.left)
+		fmt.Print(troot.root.element, " ")
+		B.preorder(troot.root.right)
 	}
 }
 
-func (B TreeNode) postorder(root *TreeNode) {
-	if root != nil {
-		B.postorder(root.left)
-		B.postorder(root.right)
-		fmt.Println(root.element)
+func (B BinaryTree) postorder(troot *BinaryTree) {
+	if troot != nil {
+		B.preorder(troot.root.left)
+		B.preorder(troot.root.right)
+		fmt.Print(troot.root.element, " ")
 	}
 }
